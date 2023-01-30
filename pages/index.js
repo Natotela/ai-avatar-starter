4fde045c7854c5bb3da7e05afa14608f4dcfce4f
I C <33201750+Natotela@users.noreply.github.com>
2023-01-30 18:38:09 +0000

    x-use-cache

--- pages/index.js
+++ pages/index.js
@@ -38,13 +38,15 @@ const Home = () => {
 
       setRetry(0);
     }
-
+    
+    const finalInput = input.replace(/itai/gi, 'ic4');
+    
     const response = await fetch('/api/generate', {
       method: 'POST',
       headers: {
         'Content-Type': 'image/jpeg',
       },
-      body: JSON.stringify({ input }),
+      body: JSON.stringify({ input: finalInput }),
     });
 
     const data = await response.json();
